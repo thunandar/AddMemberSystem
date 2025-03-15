3778074a2046a0a2e85634ec27b1717f297e0208
thunandarayemin <89308924+thunandar@users.noreply.github.com>
2025-03-15 14:08:37 +0630

    added pay list page

---
+++ Controllers/PayrollController.cs
@@ -0,0 +1,63 @@
+﻿namespace AddMemberSystem.Controllers
+{
+    public class PayrollController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDBContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public PayrollController(AppDBContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private bool IsUserLoggedIn()
+        {
+            return HttpContext.Session.GetString("loginUser") != null || Request.Cookies.ContainsKey("staySignedIn");
+        }
+
+        public IActionResult List(int pg = 1)
+        {
+            if (!IsUserLoggedIn())
+            {
+                return RedirectToAction("Index", "Account");
+            }
+
+            List<TB_Payroll> result = _context.TB_Payrolls
+               .Where(p => p.IsDeleted == false).ToList();
+
+            const int pageSize = 10;
+            if (pg < 1)
+                pg = 1;
+
+            int recsCount = result.Count();
+
+            var pager = new Pager(recsCount, pg, pageSize);
+
+            int recSkip = (pg - 1) * pageSize;
+
+            var data = result.Skip(recSkip).Take(pager.PageSize).ToList();
+
+            this.ViewBag.Pager = pager;
+
+            return View(data);
+        }
+    }
+}
+
